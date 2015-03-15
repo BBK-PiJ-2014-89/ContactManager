84e0be5de910b51622d337dc73597fa1b67cbf0e
BBK-PiJ-2014-89 <leyladrake@leylas-MacBook-Air.local>
2015-03-15 22:23:56 +0000

    added test for addNewContact() exeption hadling

--- Test/ContactManagerTest.java
+++ Test/ContactManagerTest.java
@@ -5,13 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import Implementation.ContactManagerImpl;
 import Interface.*;
 
 public class ContactManagerTest {
-	
+private ContactManager cm;
+
 	@Before
 	public void init(){
-		ContactManager cm=new ContactManagerImpl();
+		cm=new ContactManagerImpl();
 	}
 
 	@Test
@@ -59,11 +61,15 @@ public class ContactManagerTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testAddNewContact() {
-		
+	@Test (expected=NullPointerException.class)
+	public void testAddNewContact_nameExeptionHandling() {
+		cm.addNewContact(null, "new notes");
 	}
-
+	@Test (expected=NullPointerException.class)
+	public void testAddNewContact_notesExeptionHandling() {
+		cm.addNewContact("Rustam", null);
+	}
+	
 	@Test
 	public void testGetContactsIntArray() {
 		fail("Not yet implemented");
